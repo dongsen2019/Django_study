d36d54786f18b6e745c61ae7e3ff1002b649c287
dongsen <378250523@qq.com>
2024-09-26 13:47:53 +0800

    09.26 原生SQL语句大改优化

--- study_mark/01-create project and app.py
+++ study_mark/01-create project and app.py
@@ -1,6 +1,6 @@
 #  django-admin --version 查看django的版本号
 #  django-admin startproject project_name 创建一个django项目
-#  python .\manage.py runserver 172.17.19.211:8000 开启开发服务器
+#  python .\manage.py runserver 172.17.19.211:8000 开启开发服务器 / python .\manage.py runserver 172.17.19.78:8000
 #  python .\manage.py startapp food_app
 #  pip install mysqlclient 安装django与mysql交互的三方库
 #  python .\manage.py makemigrations 生成模型数据迁移文件
